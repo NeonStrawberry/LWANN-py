a2f54a2a92e2ce25ce4ede1d29a0bf5385b150a9
NeonStrawberry <96705137+NeonStrawberry@users.noreply.github.com>
2021-12-29 18:29:53 -0400

    Update neuron.py

--- src/neuron.py
+++ src/neuron.py
@@ -1,9 +1,23 @@
 class Neuron:
-    def __init__():
+    threshold = 0;
+    weights = [];
+    
+    in_cCount = 0;
+    out_cCount = 0;
+    
+    activations = [];
+    activation = [];
+    
         
 
 def compute(Neuron n):
     a = 0;
     
-    for i in range(0, n.cCount):
+    for i in range(0, n.in_cCount):
+        a += n.activations[i];
         
+    if a >= n.threshold:
+        return n.weights;
+    
+    else:
+        return [];
