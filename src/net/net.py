cacc9ad2b69c615929e264d8382c545446a0da09
NeonStrawberry <96705137+NeonStrawberry@users.noreply.github.com>
2021-12-29 18:46:13 -0400

    Update net.py

--- src/net/net.py
+++ src/net/net.py
@@ -1,1 +1,22 @@
-import 
+from neuron import *
+
+class Network:
+    n = [];
+    tempN = [];
+    
+    start = [];
+    end = [];
+    
+    done = [];
+    
+    def recursiveCompute(i):
+        
+    
+    def computeNN(ac):
+        for i in range(0, len(done)):
+            done[i] = False;
+            
+        for i in range(0, len(start)):
+            recursiveCompute(start[i]);
+            
+        n = tempN;
