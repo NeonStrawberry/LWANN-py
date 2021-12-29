d95a08a0203dd0d1fb9ff5536cc7cdb9883904f5
NeonStrawberry <96705137+NeonStrawberry@users.noreply.github.com>
2021-12-29 19:05:11 -0400

    Update net.py

--- src/net/net.py
+++ src/net/net.py
@@ -10,12 +10,19 @@ class Network:
     done = [];
     
     def recursiveCompute(x):
-        in = 0;
+        in = 0; #The indicies for the activations used in the current neuron
         
         for i in range(0, n[x].in_cCount):
-            for j in range
+            for j in range(0, len(n[n[x].index[i]].ptr)):
+                if n[n[x].index[i]].ptr[j] == x:
+                    in = j;
+                    
+        for i in range(0, n[x].in_cCount):
+            tempN[x].activations[i] = n[n[x].index[i]].activation[in[]]; #Setting up the proper activatons
     
     def computeNN(ac):
+        ret = [];
+        
         for i in range(0, len(done)):
             done[i] = False;
             
